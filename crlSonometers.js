79ba9a81f7a5de774f88ba0d4161759fd66271ad
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-06-17 16:19:38 +0200

    Add CRL_SONOMETERS data to crlSonometers.js

---
+++ crlSonometers.js
@@ -0,0 +1,20 @@
+// crlSonometers.js
+export const CRL_SONOMETERS = [
+  { id: "F118", lat: 50.505266, lon: 4.611180, address: "Rue Piconette, Sombreffe", commune: "Sombreffe" },
+  { id: "F109", lat: 50.490352, lon: 4.562389, address: "Chaussée de Charleroi, Sombreffe", commune: "Sombreffe" },
+  { id: "F108", lat: 50.486658, lon: 4.546281, address: "Avenue Brunard, Fleurus", commune: "Fleurus" },
+  { id: "F106", lat: 50.479864, lon: 4.519572, address: "Rue Beaurin et Jonet, Wangenies", commune: "Wangenies" },
+  { id: "F119", lat: 50.463214, lon: 4.479092, address: "Rue René Delhaize, Ransart", commune: "Ransart" },
+  { id: "F103", lat: 50.452386, lon: 4.415744, address: "Rue Docteur Pircard, Jumet", commune: "Jumet" },
+  { id: "F102", lat: 50.446036, lon: 4.422933, address: "Rue du Vigneron, Jumet", commune: "Jumet" },
+  { id: "F101", lat: 50.447881, lon: 4.416950, address: "Rue Bruhaute, Jumet", commune: "Jumet" },
+  { id: "F107", lat: 50.444072, lon: 4.411161, address: "Rue Maximilien Wattelar, Jumet", commune: "Jumet" },
+  { id: "F105", lat: 50.447006, lon: 4.400517, address: "Rue Sous le Bois, Roux", commune: "Roux" },
+  { id: "F104", lat: 50.442339, lon: 4.392556, address: "Rue du Chiffon Rouge, Roux", commune: "Roux" },
+  { id: "F111", lat: 50.438522, lon: 4.351964, address: "Rue de la Baille, Courcelles", commune: "Courcelles" },
+  { id: "F112", lat: 50.424653, lon: 4.357708, address: "Rue des Liserons, Goutroux", commune: "Goutroux" },
+  { id: "F117", lat: 50.431500, lon: 4.314919, address: "Rue du Terril, Forchies", commune: "Forchies" },
+  { id: "F110", lat: 50.423569, lon: 4.327381, address: "Rue Émile Vandervelde, Forchies", commune: "Forchies" },
+  { id: "F116", lat: 50.410633, lon: 4.315053, address: "Rue de l'Enseignement, Fontaine-l'Evêque", commune: "Fontaine-l'Evêque" },
+  { id: "F114", lat: 50.409831, lon: 4.277167, address: "Rue de la Source, Anderlues", commune: "Anderlues" }
+];
